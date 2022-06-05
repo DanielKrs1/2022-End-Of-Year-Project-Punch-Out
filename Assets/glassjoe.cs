e91e3510898884096d40530ffd3bd53f171f54cc
Christopher Rodriguez <ckrod16@gmail.com>
2022-06-05 14:32:08 -0400

    glassjoe(kinda)

---
+++ Assets/glassjoe.cs
@@ -0,0 +1,417 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glassjoe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite normal;
+    public Sprite normal2;
+    public Sprite normal3;
+    public Sprite normald;
+    public Sprite normald2;
+    public Sprite normald3;
+    public Sprite move;
+    public Sprite move2;
+    public Sprite move3;
+    public Sprite move4;
+    public Sprite move5;
+    public Sprite move6;
+    public Sprite prepunch;
+    public Sprite cluepunch;
+    public Sprite midpunch;
+    public Sprite punch;
+    public Sprite upperStart;
+    public Sprite upperclue;
+    public Sprite upperMid;
+    public Sprite upperEnd;
+    public Sprite blocklow;
+    public Sprite blocklow2;
+    public Sprite blockhigh;
+    public Sprite blockhigh2;
+    public Sprite blockhigh3;
+    public Sprite hitlow;
+    public Sprite hitlow2;
+    public Sprite hithigh;
+    public Sprite hithigh2;
+    public Sprite hithighr;
+    public Sprite hithighr2;
+    public Sprite dodgeHit;
+    public Sprite knockdown;
+    public Sprite knockdown2;
+    public Sprite knockdown3;
+    public Sprite getup;
+    public Sprite getup2;
+    public Sprite victory;
+    public Sprite victory2;
+    public bool punching = false;
+    public bool blockinglow = true;
+    public bool blockinghigh = false;
+    public bool specialing = false;
+    public bool onehit = false;
+    public bool counter = false;
+    public littlemac lm;
+
+    public float moveSpeed = 1f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public int frame = 0;
+    public string action = "";
+    private SpriteRenderer spriteRenderer;
+    Vector2 fp;
+    public int health = 210;
+
+    public int lowhits = 0;
+    public int highhits = 0;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if(spriteRenderer.sprite == null){
+            spriteRenderer.sprite = normal;
+        }
+        fp = rb.position;
+        moveSpeed = 1f;
+        movement.x = 0f;
+        movement.y = 0f;
+        lm = GameObject.Find("tile000 (1)").GetComponent("littlemac") as littlemac;
+        blockinglow = true;
+        blockinghigh = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lowhits == 3){
+            blockinglow = true;
+            blockinghigh = false;
+            lowhits = 0;
+        }
+        if(highhits == 3){
+            blockinghigh = true;
+            blockinglow = false;
+            highhits = 0;
+        }
+        var rand = new Random();
+        moveSpeed = 1f;
+        //lastPos = transform.position;
+        if(frame == 119){
+            frame = 0;
+        }
+        if(frame%15==0&&action.Length<2){
+            var randint = Random.Range(0, 100);
+            if(randint == 1){
+                action = "special";
+            }else if(randint<2){
+                action = "upper";
+            }else if(randint<6){
+                action = "normalPunch";
+            }
+        }
+
+        if(frame%15==0){
+            if(action.Equals("normalPunch")){
+                normalPunch();
+            }else if(action.Equals("upper")){
+                upper();
+            }else if(action.Equals("special")){
+                special();
+                rb.MovePosition(rb.position+movement);//*Time.deltaTime);  
+            }else if(action.Equals("blockLow")){
+                blockLow();
+            }else if(action.Equals("blockHigh")){
+                blockHigh();
+            }else if(action.Equals("hitLow")){
+                hitLow();
+            }else if(action.Equals("rightHit")){
+                rightHit();
+            }else if(action.Equals("leftHit")){
+                leftHit();
+            }else if(action.Equals("hitAfterDodge")){
+                hitAfterDodge();
+            }else if(action.Equals("knockdown")){
+                knockDown();
+            }else if(action.Equals("getUp")){
+                getUp();
+            }else if(action.Equals("win")){
+                win();
+            }else{
+                if(blockinghigh){
+                    if(spriteRenderer.sprite == normal){
+                        spriteRenderer.sprite = normal2;
+                    }else{
+                        spriteRenderer.sprite = normal;
+                    }
+                }else{
+                    if(spriteRenderer.sprite == normald){
+                        spriteRenderer.sprite = normald2;
+                    }else{
+                        spriteRenderer.sprite = normald;
+                    }
+                }
+            }  
+             
+        }
+         
+        frame++;  
+    }
+
+    public int count = 0;
+
+    void normalPunch(){
+        if(count == 0){
+            spriteRenderer.sprite = prepunch;
+            count++;
+        }else if(count == 1){
+            spriteRenderer.sprite = cluepunch;
+            count++;
+        }else if(count == 2){
+            spriteRenderer.sprite = prepunch;
+            count++;
+        }else if(count == 3){
+            spriteRenderer.sprite = midpunch;
+            count++;
+        }else if (count == 4){
+            spriteRenderer.sprite = normal;
+            count++;
+        }else if( count == 5){
+            punching = true;
+            spriteRenderer.sprite = punch;
+            count++;
+            if(!lm.blocking && !lm.dodging){
+                lm.health -=10;
+                if(lm.health<=0){
+                    lm.knockeddown();
+                }
+                lm.hit();
+            }
+        }else if (count == 6){
+            punching = false;
+            counter = true;
+            count++;
+        }else if(count == 7){
+                counter = false;
+                spriteRenderer.sprite = normal;
+                count = 0;
+                action = "";    
+        }
+    }
+
+    void upper(){
+        if(count == 0){
+            spriteRenderer.sprite = upperStart;
+            count++;
+        }else if (count == 1){
+            spriteRenderer.sprite = upperclue;
+            count++;
+        }else if (count == 2){
+            spriteRenderer.sprite = upperMid;
+            count++;
+        }else if (count ==3 ){
+            punching = true;
+            spriteRenderer.sprite = upperEnd;
+            count++;
+            if(!lm.blocking && !lm.dodging){
+                lm.health -=10;
+                if(lm.health<=0){
+                    lm.knockeddown();
+                }
+                lm.hit();
+            }
+        }else if (count == 4){
+            punching = false;
+            counter = true;
+            count++;      
+        }else if(count == 5){
+                counter = false;
+                spriteRenderer.sprite = normal;
+                count = 0;
+                action = "";    
+        }
+    }
+
+    void special(){
+        specialing = true;
+        if(count == 0){
+            movement.x = 0f;
+            movement.y = 1*moveSpeed;
+            spriteRenderer.sprite = normal3;
+            count++;
+        }else if (count == 1){
+            movement.y = 0f;
+            spriteRenderer.sprite = normal;
+            count++;
+        }else if (count ==2){
+            spriteRenderer.sprite = normal3;
+            count++;
+        }else if (count == 3){
+            spriteRenderer.sprite = move6;
+            count++;
+        }else if (count == 4){
+            spriteRenderer.sprite = normal3;
+            count++;
+        }else if (count == 5){
+            spriteRenderer.sprite = prepunch;
+            count++;
+        }else if(count == 6){
+            spriteRenderer.sprite = midpunch;
+            count++;
+        }else if (count == 7){
+            spriteRenderer.sprite = prepunch;
+            count++;
+        }else if (count ==8){
+            spriteRenderer.sprite = normal3;
+            count++;
+        }else if (count == 9){
+            specialing = false;
+            onehit = true;
+            spriteRenderer.sprite = normald;
+            count++;
+            movement.x = 0f;
+            movement.y = -1*moveSpeed;
+            rb.position  = fp;
+            spriteRenderer.sprite = normal3;
+            count++;
+        }else{
+            specialing = false;
+            movement.y = 0f;
+            onehit = false;
+            count-=10;
+            upper();
+            if(count!=0){
+                count+=10;
+            }
+        }
+    }
+
+    public void blockLow(){
+        action = "blockLow";
+        if(count == 0){
+            spriteRenderer.sprite = blocklow;
+            count++;
+        }if(count == 1){
+            spriteRenderer.sprite = blocklow2;
+            count++;
+        }else{
+            spriteRenderer.sprite = normal;
+            count = 0;
+            action = "";
+        }
+    }
+
+    public void blockHigh(){
+        action = "blockHigh";
+        if(count == 0){
+            spriteRenderer.sprite = blockhigh;
+            count++;
+        }if(count == 1){
+            spriteRenderer.sprite = blockhigh2;
+            count++;
+        }else if(count == 3){
+            spriteRenderer.sprite = blockhigh3; 
+        }else{
+            spriteRenderer.sprite = normal;
+            count = 0;
+            action = "";
+        }
+    }
+
+    public void hitLow(){
+        lowhits++;
+        action = "hitLow";
+        if(count <= 2){
+            spriteRenderer.sprite = hitlow;
+            count++;
+        }else{
+            spriteRenderer.sprite = normal;
+            count = 0;
+            action = "";
+        }
+    }
+
+    public void rightHit(){
+        highhits++;
+        action = "rightHit";
+        if(count == 0){
+            spriteRenderer.sprite = hithighr;
+            count++;
+        }if(count == 1){
+            spriteRenderer.sprite = hithighr2;
+            count++;
+        }else{
+            spriteRenderer.sprite = normal;
+            count = 0;
+            action = "";
+        }
+    }
+
+    public void leftHit(){
+        highhits++;
+        action = "leftHit";
+        if(count == 0){
+            spriteRenderer.sprite = hithigh;
+            count++;
+        }if(count == 1){
+            spriteRenderer.sprite = hithigh2;
+            count++;
+        }else{
+            spriteRenderer.sprite = normal;
+            count = 0;
+            action = "";
+        }
+    }
+
+    int hits = 7;
+    public void hitAfterDodge(){
+        action = "hitAfterDodge";
+        if(hits>0){
+            spriteRenderer.sprite = dodgeHit;
+        }else{
+            action = "";
+            spriteRenderer.sprite = normal;
+            hits = 7;
+        }
+    }
+
+    public void knockDown(){
+        action = "knockDown";
+        if(count <=2){
+            spriteRenderer.sprite = knockdown;
+            count++;
+        }else if (count <=5){
+            spriteRenderer.sprite = knockdown2;
+            count++;
+        }else{
+            spriteRenderer.sprite = knockdown3;
+        }
+    }
+
+    public void getUp(){
+        action = "getUp";
+        if(count <=2){
+            spriteRenderer.sprite = getup;
+            count++;
+        }else if (count <=5){
+            spriteRenderer.sprite = getup2;
+            count++;
+        }else{
+            spriteRenderer.sprite = normal;
+            count = 0;
+            action = "";
+        }
+    }
+
+    public void win(){
+        action = "win";
+        if(count <= 3){
+            spriteRenderer.sprite = victory;
+            count++;
+        }else{
+            spriteRenderer.sprite = victory2;
+        }
+    }
+
+    public void redo(){
+        count = 0;
+        action = "";
+    }
+}
---
+++ Assets/littlemac.cs
@@ -0,0 +1,439 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class littlemac : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 1f;
+
+    public Sprite normal;
+    public Sprite normal2;
+    public Sprite leftPunch;
+    public Sprite leftPunchMid;
+    public Sprite leftPunchStart;
+    public Sprite rightPunch;
+    public Sprite rightPunchMid;
+    public Sprite rightPunchStart;
+    public Sprite rightUppercutStart;
+    public Sprite rightUppercutMid;
+    public Sprite rightUppercutEnd;
+    public Sprite leftUppercutStart;
+    public Sprite leftUppercutMid;
+    public Sprite leftUppercutEnd;
+    public Sprite blockStart;
+    public Sprite block;
+    public Sprite blockBroken;
+    public Sprite dodgeLeft;
+    public Sprite dodgeLeftStart;
+    public Sprite dodgeLeftEnd;
+    public Sprite dodgeRight;
+    public Sprite dodgeRightStart;
+    public Sprite dodgeRightEnd;
+    public Sprite hit1;
+    public Sprite hit2;
+    public Sprite knockedDown;
+
+    public glassjoe enemy;
+    public int health = 90;
+
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public int frame = 0;
+    public string action = "";
+    private SpriteRenderer spriteRenderer;
+    Vector2 fp;
+
+    public bool punchlow = false;
+    public bool punchhigh = false;
+    public bool dodging = false;
+    public bool blocking = false;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if(spriteRenderer.sprite == null){
+            spriteRenderer.sprite = normal;
+        }
+        fp = rb.position;
+        moveSpeed = 1f;
+        enemy = GameObject.Find("tile00b").GetComponent("glassjoe") as glassjoe;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveSpeed = 1f;
+        //lastPos = transform.position;
+        if(frame == 119){
+            frame = 0;
+        }
+        if(frame%15==0&&action.Length<2){
+            
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                action = "dodgeRight";
+            }else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                action = "dodgeLeft";       
+            }else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                if(Input.GetKey(KeyCode.Z)){
+                    action = "upPunchLeft";
+                }else if(Input.GetKey(KeyCode.X)){
+                    action = "upPunchRight";
+                }
+            }else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                action = "block";
+            }else if(Input.GetKey(KeyCode.Z)){
+                action = "punchLeft";
+            }else if(Input.GetKey(KeyCode.X)){
+                action = "punchRight";
+            }
+        }
+
+        if(frame%15==0){
+            if(action.Equals("dodgeLeft")){
+                leftDodge();
+            }else if(action.Equals("dodgeRight")){
+                rightDodge();
+            }else if(action.Equals("upPunchLeft")){
+                uppercutLeft();
+            }else if(action.Equals("upPunchRight")){
+                uppercutRight();
+            }else if(action.Equals("block")){
+                bloc();
+            }else if(action.Equals("punchLeft")){
+                punchLeft();
+            }else if(action.Equals("punchRight")){
+                punchRight();
+            }else if (action.Equals("hit")){
+                hit();
+            }else{
+                if(spriteRenderer.sprite == normal){
+                    spriteRenderer.sprite = normal2;
+                }else{
+                    spriteRenderer.sprite = normal;
+                }
+                movement.x = 0f;
+                movement.y = 0f;
+            }  
+            rb.MovePosition(rb.position+movement);//*Time.deltaTime);   
+        }
+         
+        frame++;  
+    }
+
+    public bool back = false;
+
+    void leftDodge(){
+        if(spriteRenderer.sprite == dodgeLeft){
+            if(back){
+                dodging = false;
+                spriteRenderer.sprite = dodgeLeftStart;
+                movement.x = moveSpeed/2;
+                movement.y = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }else{
+                spriteRenderer.sprite = dodgeLeftEnd;
+                movement.x = -1*moveSpeed/2;
+                movement.y = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }
+        }else if(spriteRenderer.sprite == dodgeLeftEnd){
+            back = true;
+            spriteRenderer.sprite = dodgeLeft;
+            
+            movement.x = moveSpeed/2;
+            movement.y = 0f;
+            //rb.MovePosition(rb.position + movement * Time.deltaTime);
+        }else{
+            if(back){
+                back = false;
+                spriteRenderer.sprite = normal;
+                action = "";
+                rb.position = fp;
+                movement.x = 0;
+                movement.y = 0;
+            }else if(spriteRenderer.sprite == dodgeLeftStart){
+                dodging = true;
+                spriteRenderer.sprite = dodgeLeft;
+                movement.x = -1*moveSpeed/2;
+                movement.y = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }else{
+                spriteRenderer.sprite = dodgeLeftStart;                
+            }
+        }
+    }
+
+    void rightDodge(){
+        if(spriteRenderer.sprite == dodgeRight){
+            if(back){
+                dodging = false;
+                spriteRenderer.sprite = dodgeRightStart;
+                movement.x = -1*moveSpeed/2;
+                movement.y = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }else{
+                spriteRenderer.sprite = dodgeRightEnd;
+                movement.x = moveSpeed/2;
+                movement.y = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }
+        }else if(spriteRenderer.sprite == dodgeRightEnd){
+            back = true;
+            spriteRenderer.sprite = dodgeRight;
+            movement.x = -1*moveSpeed/2;
+            movement.y = 0f;
+            //rb.MovePosition(rb.position + movement * Time.deltaTime);
+        }else{
+            if(back){
+                back = false;
+                spriteRenderer.sprite = normal;
+                action = "";
+                rb.position = fp;
+                movement.x = 0;
+                movement.y = 0;
+            }else if(spriteRenderer.sprite == dodgeRightStart){
+                dodging = true;
+                spriteRenderer.sprite = dodgeRight;
+                movement.x = moveSpeed/2;
+                movement.y = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }else{
+                spriteRenderer.sprite = dodgeRightStart;
+            }
+        }
+    }
+
+    void uppercutLeft(){
+        if(spriteRenderer.sprite == leftUppercutMid){
+            if(back){
+                spriteRenderer.sprite = leftUppercutStart;
+                movement.x = 0;
+                movement.y = 0;
+            }else{
+                punchhigh = true;
+                spriteRenderer.sprite = leftUppercutEnd;
+                movement.y = moveSpeed;
+                movement.x = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }
+        }else if(spriteRenderer.sprite == leftUppercutEnd){
+            back = true;
+            spriteRenderer.sprite = leftUppercutMid;
+            movement.y = -1*moveSpeed;
+            movement.x = 0f;
+            punchhigh = false;
+            if(enemy.blockinghigh){
+                enemy.blockHigh();
+            }else if (!(enemy.specialing)){
+                enemy.health-=10;
+                if(enemy.onehit){
+                    enemy.health = 0;
+                    enemy.knockDown();
+                }else{
+                    if(enemy.counter){
+                        enemy.hitAfterDodge();
+                    }else{
+                        enemy.leftHit();
+                    }
+                } 
+                if(enemy.health<=0){
+                    enemy.knockDown();
+                }     
+            }
+            //rb.MovePosition(rb.position + movement * Time.deltaTime);
+        }else{
+            if(back){
+                back = false;
+                spriteRenderer.sprite = normal;
+                action = "";
+                rb.position = fp;
+                movement.x = 0;
+                movement.y = 0;
+            }else if(spriteRenderer.sprite == leftUppercutStart){
+                spriteRenderer.sprite = leftUppercutMid;
+            }else{
+                spriteRenderer.sprite = leftUppercutStart;
+            }
+        }
+    }
+
+    void uppercutRight(){
+        if(spriteRenderer.sprite == rightUppercutMid){
+            if(back){
+                spriteRenderer.sprite = rightUppercutStart;
+                movement.x = 0;
+                movement.y = 0;
+            }else{
+                punchhigh = true;
+                spriteRenderer.sprite = rightUppercutEnd;
+                movement.y = moveSpeed;
+                movement.x = 0f;
+                //rb.MovePosition(rb.position + movement * Time.deltaTime);
+            }
+        }else if(spriteRenderer.sprite == rightUppercutEnd){
+            back = true;
+            spriteRenderer.sprite = rightUppercutMid;
+            movement.y = -1*moveSpeed;
+            movement.x = 0f;
+            punchhigh = false;
+            if(enemy.blockinghigh){
+                enemy.blockHigh();
+            }else if (!(enemy.specialing)){
+                enemy.health-=10;
+                if(enemy.onehit){
+                    enemy.health = 0;
+                    enemy.knockDown();
+                }else{
+                    if(enemy.counter){
+                        enemy.hitAfterDodge();
+                    }else{
+                        enemy.rightHit();
+                    }
+                } 
+                if(enemy.health<=0){
+                    enemy.knockDown();
+                }     
+            }
+            //rb.MovePosition(rb.position + movement * Time.deltaTime);
+        }else{
+            if(back){
+                back = false;
+                spriteRenderer.sprite = normal;
+                action = "";
+                rb.position = fp;
+                movement.x = 0;
+                movement.y = 0;
+            }else if(spriteRenderer.sprite == rightUppercutStart){
+                spriteRenderer.sprite = rightUppercutMid;
+            }else{
+                spriteRenderer.sprite = rightUppercutStart;
+            }
+        }
+    }
+
+    void bloc(){
+        if(spriteRenderer.sprite == block){
+            if(!(Input.GetKey(KeyCode.DownArrow))){
+                blocking = false;
+                spriteRenderer.sprite = normal;
+                action = "";
+                return;
+            }
+        }else{
+            if(spriteRenderer.sprite == blockStart){
+                spriteRenderer.sprite = block;
+                blocking = true;
+            }else{
+                spriteRenderer.sprite= blockStart;
+            }
+        }
+    }
+
+    void punchLeft(){
+        if(spriteRenderer.sprite == leftPunchMid){
+            if(back){
+                spriteRenderer.sprite = leftPunchStart;
+            }else{
+                spriteRenderer.sprite = leftPunch;
+                punchlow = true;
+            }
+        }else if(spriteRenderer.sprite == leftPunch){
+            back = true;
+            spriteRenderer.sprite = leftPunchMid;
+            punchlow = false;
+            if(enemy.blockinglow){
+                enemy.blockLow();
+            }else if (!(enemy.specialing)){
+                enemy.health-=10;
+                if(enemy.counter){
+                    enemy.hitAfterDodge();
+                }else{
+                    enemy.hitLow();
+                }
+                if(enemy.health<=0){
+                    enemy.knockDown();
+                }   
+            }              
+        }else{
+            if(back){
+                back = false;
+                spriteRenderer.sprite = normal;
+                action = "";
+            }else if(spriteRenderer.sprite == leftPunchStart){
+                spriteRenderer.sprite = leftPunchMid;
+            }else{
+                spriteRenderer.sprite = leftPunchStart;
+            }
+        }
+    }
+
+    void punchRight(){
+        if(spriteRenderer.sprite == rightPunchMid){
+            if(back){
+                spriteRenderer.sprite = rightPunchStart;
+            }else{
+                spriteRenderer.sprite = rightPunch;
+                punchlow = true;
+            }
+        }else if(spriteRenderer.sprite == rightPunch){
+            back = true;
+            spriteRenderer.sprite = rightPunchMid;
+            punchlow = false;
+            if(enemy.blockinglow){
+                enemy.blockLow();
+            }else if (!(enemy.specialing)){
+                enemy.health-=10;
+                if(enemy.counter){
+                    enemy.hitAfterDodge();
+                }else{
+                    enemy.hitLow();
+                }
+                if(enemy.health<=0){
+                    enemy.knockDown();
+                }   
+            }
+        }else{
+            if(back){
+                back = false;
+                spriteRenderer.sprite = normal;
+                action = "";
+            }else if(spriteRenderer.sprite == rightPunchStart){
+                spriteRenderer.sprite = rightPunchMid;
+            }else{
+                spriteRenderer.sprite = rightPunchStart;
+            }
+        }
+    }
+
+    public void hit(){
+        action = "hit";
+        if(spriteRenderer.sprite == hit1){
+            spriteRenderer.sprite = hit2;
+        }else{
+            if(spriteRenderer.sprite = hit2){
+                spriteRenderer.sprite = normal;
+                action = "";
+            }else{
+                spriteRenderer.sprite = hit1;
+            }
+        }
+    }
+
+    public void knockeddown(){
+        if(spriteRenderer.sprite == hit1){
+            spriteRenderer.sprite = hit2;
+        }else{
+            if(spriteRenderer.sprite = hit2){
+                spriteRenderer.sprite = knockedDown;
+            }else{
+                spriteRenderer.sprite = hit1;
+            }
+        }
+    }
+
+}
