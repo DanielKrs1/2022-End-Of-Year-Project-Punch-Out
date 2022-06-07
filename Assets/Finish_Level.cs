18248ebe4a377117e85ad4365c6e9541c7d5f010
jadenmanuel <jadenmanuel2006@gmail.com>
2022-06-07 08:42:20 -0400

    Levels and  scripts

---
+++ Assets/Finish_Level.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Finish_Level : MonoBehaviour
+{
+    // Start is called before the first frame update
+   private void Finished(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+       
+    }
+}
---
+++ Assets/PointKeeper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointKeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static int points;
+    void Start()
+    {
+        points = 0;
+    }
+
+    // Update is called once per frame
+    void updatePoints(){
+        points+=1;
+        
+    }
+}
